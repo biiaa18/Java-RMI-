c4d5ea20ce3d1f8f689a0a15ff8694c17d478240
biiaa18 <splunch18@gmail.com>
2026-09-16 20:37:27 +0200

    added input file parser

--- solution/src/com/ass1/client/Client.java
+++ solution/src/com/ass1/client/Client.java
@@ -1,15 +1,25 @@
 package com.ass1.client;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 import com.ass1.server.ProxyInterface;
 import com.ass1.server.ServerInterface;
+import com.ass1.client.Request;
 
 public class Client {
+
     public static void main() {
+        List<Request> requests=new ArrayList<>();
+
+        //todo: should this be a thread as well?
         try {
             Registry registry = LocateRegistry.getRegistry(1099);
             ServerInterface server = (ServerInterface) registry.lookup("server");
@@ -18,6 +28,28 @@ public class Client {
             System.out.println(proxyServer.GetServer());
         } catch (RemoteException | NotBoundException e) {
             e.printStackTrace();
+        }
+
+        //input file with requests from client
+        File file = new File("exercise_1_input.txt");
+
+        try (Scanner scanner1 = new Scanner(file);){
+            while (scanner1.hasNextLine()){
+                String line= scanner1.nextLine().trim();
+                if(!line.isEmpty()){
+
+                    requests.add(Request.readLine(line));
+                    //System.out.println("file:"+requests.size());
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        //just test
+        for(Request request: requests){
+            System.out.println("'"+request.getMethodName()+"'  '"+request.getArgList()+"'  '"+request.getZoneNumber()+"'");
         }
     }
 }
---
+++ solution/src/com/ass1/client/Request.java
@@ -0,0 +1,82 @@
+package com.ass1.client;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Request {
+    private String methodName=null;
+    private List<String> argList=null;
+    private Integer zoneNumber=null;
+
+    public String getMethodName(){return methodName;}
+    public List<String> getArgList(){return argList;}
+
+    public Integer getZoneNumber() {
+        return zoneNumber;
+    }
+
+    ///for making new request object, when reading request line
+    public Request(String inputMethod, List<String> argumentsList, Integer zoneN){
+        this.methodName=inputMethod;
+        this.argList=argumentsList;
+        this.zoneNumber=zoneN;
+    }
+
+    ///read line and split it into arguments
+    public static Request readLine(String line){
+        //split line by space first
+        List<String> requestArguments=new ArrayList<>(Arrays.asList(line.split(" ")));
+        //always first
+        String methodNamee=requestArguments.removeFirst();
+        //always last
+        Integer clientZoneNumber=Integer.parseInt(requestArguments.removeLast().split(":")[1]);
+        //based on method, number of arguments change, but city can also be of several words
+        List<String> arguments= splitArguments(methodNamee,requestArguments);
+        return new Request(methodNamee,arguments, clientZoneNumber);
+    }
+
+    ///split arguments correctly (case: city of 2 words)
+    //getPopulationofCountry : 1   // has name
+    //getNumberofCities : 3        //has name
+    //getNumberofCountries : 3
+    //getNumberofCountriesMM : 3
+    private static List<String> splitArguments(String methodNamee,List<String> line){
+        //case: no names
+        if(methodNamee.equals("getNumberofCountriesMM") || methodNamee.equals("getNumberofCountries")){
+            return line;
+        }
+
+        List<String> returnList = new ArrayList<>();
+        //case: 1 argument: name
+        if(methodNamee.equals("getPopulationofCountry")){
+            StringBuilder completeArg=new StringBuilder();
+            for(int i=0; i< line.size(); i++){
+                completeArg.append(line.get(i));
+                if(i<line.size()-1){
+                    completeArg.append(" ");
+                }
+
+            }
+            returnList.add(completeArg.toString());
+            return returnList;
+        }
+        //case: 3 arguments, name
+        else if(methodNamee.equals("getNumberofCities")){
+            StringBuilder completeArg=new StringBuilder();
+            for(int i=0; i<line.size()-2; i++){
+                completeArg.append(line.get(i));
+                if(i<line.size()-3){
+                    completeArg.append(" ");
+                }
+            }
+            returnList.add(completeArg.toString());
+            returnList.add(line.get(line.size()-2));
+            returnList.add(line.get(line.size()-1));
+        }
+        else{
+            throw new IllegalArgumentException("method doesn't exist: "+methodNamee);
+        }
+        return returnList;
+    }
+}
